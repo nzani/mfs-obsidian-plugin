d03419f5e7c743e28c1007dfa0eede2b41437326
Nico <nrgzani@gmail.com>
2023-08-07 21:59:23 -0400

    found resource path for map image

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Obsidian Map File System Plugin
+# The Map File System: Obsidian Plugin
 
+Welcome to the Map File System: the file system built around maps! This project
+was designed to help a DM/GM build out their world, with maps as the central 
+framework for linking files, documents, and information. 
+
+The idea is simple: imagine that your top directory has a map associated with
+it, the map of your world. You can then populate this world map with pins, which
+either represent documents or subdirectories, which contain information about
+something within your map at the pins location. With this, a DM/GM can fill out
+their world by _location_ and use their maps to do so. 
+
+## Development Setup
+
+To develop, you will need to have [Obsidian](https://obsidian.md/), [Node.js]
+(https://nodejs.org/en), and [Node Package Manager]
+
+- add directory called `vault` to project
+- open this up in obsidian to create the test vault
+- add in a `plugins` folder to `.obsidian/`, and add a `mfs-obsidian` directory
+    - copy `main.js` and `manifest.json` here when building
--- src/view/view.tsx
+++ src/view/view.tsx
@@ -1,4 +1,4 @@
-import { FileView, WorkspaceLeaf, Notice, TFile, FileSystemAdapter } from "obsidian"
+import { arrayBufferToBase64, FileView, WorkspaceLeaf, Notice, TFile, FileSystemAdapter } from "obsidian"
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { ReactView } from "./ReactView"
@@ -10,6 +10,7 @@ export class MapView extends FileView {
 
   map: string = ""
   name: string = "Unnamed Map"
+  mapAbsPath: string = ""
 
   constructor(leaf: WorkspaceLeaf) {
     super(leaf)
@@ -20,27 +21,29 @@ export class MapView extends FileView {
   async onLoadFile(file : TFile) {
 
     let vault = this.app.vault
-    
-    let fsa = new FileSystemAdapter()
-
     let data = JSON.parse(await vault.read(file))
 
+    // TODO: do these need to be saved? will they be used anywhere else?
     this.map = data["map"]
     this.name = data["name"]
 
-    let map = vault.getAbstractFileByPath(this.map)!.path
-
+    // TODO: better way to find resource path of the map?
     let files = vault.getFiles()
+    for(var i = 0; i < files.length; i++) {
+      if (files[i].path === this.map) {
+        this.mapAbsPath = vault.getResourcePath(files[i])
+      }
+    }
 
-    // function hexToBase64(str) {
-    //   return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
-    // }
+    // create React element after file loaded
+    const root = createRoot(this.containerEl.children[1])
 
-    new Notice(this.map)
-    new Notice(vault.configDir)
+    root.render(
+      <React.StrictMode>
+        <ReactView src={this.mapAbsPath}/>
+      </React.StrictMode>
+    )
 
-    new Notice(vault.getResourcePath(file))
-    new Notice(vault.getRoot().path)
   }
 
   getViewType() {
@@ -52,15 +55,7 @@ export class MapView extends FileView {
   }
 
   async onOpen() {
-    const root = createRoot(this.containerEl.children[1])
-
-    let imgData = 'data:image/jpeg;base64,' + (await this.app.vault.adapter.read(this.map))
-
-    root.render(
-      <React.StrictMode>
-        <ReactView file={imgData}/>
-      </React.StrictMode>
-    )
+    
       
   }
 
